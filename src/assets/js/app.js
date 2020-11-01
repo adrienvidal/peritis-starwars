e10a028bcff2bf18486b13f34d114fef8f6e6218
Adrien Vidal <adriensg.vidal@gmail.com>
2020-11-01 03:42:05 +0100

    nav js

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,44 @@
-console.log('app')
+const app = {
+  state: {
+    isOpen: false
+  },
 
-const test = 123
+  init () {
+    this.navMobile()
+  },
+
+  navMobile () {
+    const header = document.querySelector('header')
+    const burgerMenu = document.querySelector('.burger-menu')
+    const bodyTag = document.querySelector('body')
+    const desktopSize = window.matchMedia('(min-width: 768px)')
+
+    burgerMenu.addEventListener('click', () => {
+      if (!this.state.isOpen) {
+        this.showMenu(header, bodyTag)
+      } else {
+        this.closeMenu(header, bodyTag)
+      }
+    })
+
+    window.addEventListener('resize', () => {
+      if (desktopSize.matches) {
+        this.closeMenu(header, bodyTag)
+      }
+    })
+  },
+
+  showMenu (header, bodyTag) {
+    header.classList.add('active')
+    bodyTag.style.overflow = 'hidden'
+    this.state.isOpen = true
+  },
+
+  closeMenu (header, bodyTag) {
+    header.classList.remove('active')
+    bodyTag.style.overflow = ''
+    this.state.isOpen = false
+  }
+}
+
+app.init()
